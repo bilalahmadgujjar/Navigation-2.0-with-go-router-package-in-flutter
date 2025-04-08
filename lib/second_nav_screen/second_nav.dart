8d90b82c9c08c83b90580a2e375a8b64738bc47e
bilalahmadgujjar <153160047+bilalahmadgujjar@users.noreply.github.com>
2025-04-08 17:01:30 +0500

    second_nav fix app exit

--- lib/second_nav_screen/second_nav.dart
+++ lib/second_nav_screen/second_nav.dart
@@ -12,19 +12,31 @@ class SecondNav extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white,
-      body: navigationShell, // Shows the current selected screen
-      bottomNavigationBar: BottomNavigationBar(
-        currentIndex: navigationShell.currentIndex,
-        onTap: _onItemTapped,
-        selectedItemColor: Colors.blue,
-        unselectedItemColor: Colors.grey,
-        items: const [
-          BottomNavigationBarItem(icon: Icon(Icons.access_time_outlined), label: 'Home'),
-          BottomNavigationBarItem(icon: Icon(Icons.production_quantity_limits), label: 'Profile'),
-          BottomNavigationBarItem(icon: Icon(Icons.ac_unit), label: 'Settings'),
-        ],
+    return WillPopScope(
+      onWillPop: () async {
+        // If we are not on the home tab, navigate back to home instead of exiting
+        if (navigationShell.currentIndex != 0) {
+          print('this is not back');
+          _onItemTapped(0); // Go to the home tab
+          return Future.value(false); // Prevent the app from exiting
+        }
+        print('this is the back');
+        return Future.value(true); // Allow the app to exit if on home tab
+      },
+      child: Scaffold(
+        backgroundColor: Colors.white,
+        body: navigationShell, // Shows the current selected screen
+        bottomNavigationBar: BottomNavigationBar(
+          currentIndex: navigationShell.currentIndex,
+          onTap: _onItemTapped,
+          selectedItemColor: Colors.blue,
+          unselectedItemColor: Colors.grey,
+          items: const [
+            BottomNavigationBarItem(icon: Icon(Icons.access_time_outlined), label: 'Home'),
+            BottomNavigationBarItem(icon: Icon(Icons.production_quantity_limits), label: 'Profile'),
+            BottomNavigationBarItem(icon: Icon(Icons.ac_unit), label: 'Settings'),
+          ],
+        ),
       ),
     );
   }
